1055bcf060702992db8941c92a55f0c6e3de34e9
zhafir <zzzhafir75@gmail.com>
2020-07-09 17:11:29 +0800

    Add generateNewGrid and deleteAllCells functions
    Edit generateGrid to accept only x as a parameter. Move width parameter to
    a global variable. Change clearBtn event listener to generateNewGrid.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const gridCells = generateGrid(16, 400);
+const gridTotalWidth = 600;
+
+let gridCells = generateGrid(16);
 const clearBtn = document.querySelector('#clear-btn');
 
-clearBtn.addEventListener('click', clearCells);
+clearBtn.addEventListener('click', generateNewGrid);
 
-//Generate a grid of divs within grid-container 
-//with x rows and columns, and total width of w px
+//Generate a grid of divs within grid-container with x rows and columns
 //Returns an array of grid-cells
-function generateGrid(x, w){
+function generateGrid(x){
     //space between cells in px
-    const spacing = 2;
+    const spacing = x < 32 ? 1 : 0;
     const gContainer = document.querySelector('.grid-container');
-    const widthPerCell = (w - (x - 1) * spacing) / x;
+    const widthPerCell = (gridTotalWidth - (x - 1) * spacing) / x;
 
     let cellsArr = [];
 
@@ -32,10 +33,35 @@ function generateGrid(x, w){
     return cellsArr;
 }
 
+//Generates a new empty grid based on the user num via a prompt
+function generateNewGrid(){
+    let num;
+    num = parseInt(prompt("New grid resolution (squares per side, max. limit : 64)", 16));
+    
+    //Rejects odd user input
+    while(isNaN(num) || num > 64 || num < 2){
+        num = prompt("Invalid num, please try again. (Max. limit : 64)", 16);
+    }
+
+    deleteAllCells();
+    gridCells = generateGrid(num);
+}
+
+//Clears all the cells to the normal whitesmoke colour
 function clearCells(){
     gridCells.forEach(cell => {
         cell.style.backgroundColor = 'whitesmoke';
     });
+}
+
+//Deletes all the cell divs from the DOM
+function deleteAllCells(){
+    clearCells();
+    gridCells.forEach((cell) => {
+        cell.parentNode.removeChild(cell);
+    });
+
+    gridCells = [];
 }
 
 function cellOnHover(e){
