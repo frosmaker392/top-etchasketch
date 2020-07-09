aa0749425084fb810a27a7492dbb87ecfe10ea2c
zhafir <zzzhafir75@gmail.com>
2020-07-09 15:54:56 +0800

    Add mouseOver fuctionality, add transition delay to cells

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const gridCells = generateGrid(16, 16, 15);
+
 //Generate a grid of divs within grid-container 
 //with x rows, y columns, and cell width w (px)
+//Returns an array of grid cells
 function generateGrid(x, y, w){
     //space between cells in px
     const spacing = 2;
     const gContainer = document.querySelector('.grid-container');
+
+    let cellsArr = [];
 
     gContainer.setAttribute('style',
      `grid-template-columns: repeat(${x}, ${w + spacing}px);
@@ -14,9 +19,13 @@ function generateGrid(x, y, w){
         cell.classList.add('grid-cell');
         cell.style.width = w + 'px';
         cell.style.height = w + 'px';
+        cell.addEventListener('mouseover', cellOnHover);
 
         gContainer.appendChild(cell);
+        cellsArr.push(cell);
     }
 }
 
-generateGrid(16, 16, 15);
+function cellOnHover(e){
+    e.target.style.backgroundColor = 'black';
+}
